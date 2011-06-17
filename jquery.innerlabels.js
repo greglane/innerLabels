c48955329d9156b66d72f8b6166f5185d3a0d72a
rupert654 <rupert@madden-abbott.com>
2011-06-16 21:21:17 -0700

    Missed a bracket and made indentation consistent

--- jquery.innerlabels.js
+++ jquery.innerlabels.js
@@ -1,12 +1,13 @@
 (function($) {
     $.fn.innerLabels = function() {
         return this.each(function() {
-          var label = $(this).siblings('label');
-          if($(this).val()) label.hide();
-          $(this).bind({
-            focus: function() { label.hide(); },
-            blur: function() { if(!$(this).val()) label.show(); }
-            change: function() { if(!$(this).val()) label.show(); }
-          });
+            var label = $(this).siblings('label');
+            if($(this).val()) label.hide();
+            $(this).bind({
+                focus: function() { label.hide(); },
+                blur: function() { if(!$(this).val()) label.show(); }
+                change: function() { if(!$(this).val()) label.show(); }
+            });
         });
+    }
 })(jQuery);
